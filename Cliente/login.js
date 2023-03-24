65a0920d64c5ec9d77a89d4c17f6df358856b844
DavidAguilar2022 <3512037@alu.murciaeduca.es>
2023-03-24 19:36:58 +0100

    cambios en las clases auth y auth.class

---
+++ Cliente/login.js
@@ -0,0 +1,13 @@
+const formulario=document.getElementsByTagName("form")
+let correo=doccument.querySelector("#correo")
+let password=document.querySelector("#password")
+
+formulario.addEventListener("submit",function(event){
+
+    //captamos el envio de datos
+    event.preventDefault();
+
+    
+
+
+})
