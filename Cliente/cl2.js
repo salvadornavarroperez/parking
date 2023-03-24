5f0ee48fc37c6ef123d1bd2a3762997378f56a0f
salva311 <4167122@alu.murciaeduca.es>
2023-03-24 13:24:36 +0100

    api socios y inicio con opcion de hacerse socio

---
+++ Cliente/cl2.js
@@ -0,0 +1,30 @@
+var menu = document.getElementById("menu");
+var socioLista = document.createElement("li");
+var enlaceSocio = document.createElement("a");
+enlaceSocio.textContent = "Hacerse socio";
+socioLista.id="esSocio";
+// formulario aun por hacer
+enlaceSocio.href="index-socio.html" 
+socioLista.appendChild(enlaceSocio);
+
+// variable usuario aun por definir por variable de storage, arreglo temporal
+var user = 2;
+
+// ver si el menu esta con la pregunta de hacerse socio
+var hacerseSocio = document.getElementById("menu");
+
+fetch("http://localhost/Proyecto/parking/socios.php?Id_usuario=" + user)
+.then(respuesta=>respuesta.json())
+.then(datos=>{
+
+// comprobar que recibimos datos de socio o no
+    var socios = Array.from(datos.socios);
+    console.log(socios.length)
+
+    // si no es socio añadimos al menu la opcion de hacerse socio, si no, la quitamos si aparece
+    if(socios.length == 0) {
+        menu.append(socioLista);
+    } else if( socios.length > 0 && hacerseSocio != null) {
+        socioLista.removeChild(enlaceSocio);
+    }
+}) 
