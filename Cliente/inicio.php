ca16337957a130acc05f768b1a07fb928f64bbb8
salva311 <4167122@alu.murciaeduca.es>
2023-05-09 17:59:30 +0200

    cambios responsive

--- Cliente/inicio.php
+++ Cliente/inicio.php
@@ -13,13 +13,18 @@
 <body>
     <div id="user"></div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <ul class="navbar-nav mr-auto" id = "menu">
-            <li class="nav-item"><a class="nav-link" href="inicio.php">Inicio</a></li>                       
-            <li class="nav-item"><a class="nav-link" href="vehiculos/registrarVehiculo.html">Registro Vehiculo</a></li>
-            <li class="nav-item"><a class="nav-link" href="vehiculos/misVehiculos.html">Mis vehiculos</a></li>
-            <li class="nav-item"><a class="nav-link" href="perfil_usuario.html">Mi perfil</a></li>            
-            <li class="nav-item"><a class="nav-link" href="reservas/misReservas.html">Mis Reservas</a></li>
-        </ul>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuCollapse" aria-controls="menuCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="menuCollapse">
+            <ul class="navbar-nav mr-auto" id="menu">
+                <li class="nav-item"><a class="nav-link" href="inicio.php">Inicio</a></li>                       
+                <li class="nav-item"><a class="nav-link" href="vehiculos/registrarVehiculo.html">Registro Vehiculo</a></li>
+                <li class="nav-item"><a class="nav-link" href="vehiculos/misVehiculos.html">Mis vehiculos</a></li>
+                <li class="nav-item"><a class="nav-link" href="perfil_usuario.html">Mi perfil</a></li>            
+                <li class="nav-item"><a class="nav-link" href="reservas/misReservas.html">Mis Reservas</a></li>
+            </ul>
+        </div>
     </nav>
     <!-- Contenido de la página -->
     <div class="container">
@@ -37,5 +42,8 @@
     </main>
     <!-- Enlace al archivo de JavaScript con atributo defer para carga asíncrona -->
     <script type="module" defer src="inicio.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
